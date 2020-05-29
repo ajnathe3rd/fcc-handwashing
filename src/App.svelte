840af387547a3f3b351694456615d606b3753a02
Dileep Dasari <dileep.dasari@salesforce.com>
2020-05-28 23:07:59 -0700

    got sound to work

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
-    import Timer from './Timer.svelte'
-    import HowTo from './HowTo.svelte'
+    import Timer from './Timer.svelte';
+    import HowTo from './HowTo.svelte';
+    let audio;
+    function timerEnds(e) {
+        audio.play();
+    } 
 </script>
 <style>
     h1, h3 {
@@ -8,9 +12,13 @@
     }
 </style>
 <h1>Handwashing App</h1>
-<Timer />
+<Timer on:end={timerEnds} />
 <HowTo />
 <h3>
     <a href="https://www.who.int/gpsc/media/how_to_handwash_lge.gif">Picture Source</a>
     <a href="https://freesound.org/people/metrostock99/sounds/345086">Sound Source</a>
 </h3>
+
+<audio bind:this={audio}>
+    <source src="build/sound.wav">
+</audio>
--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,7 +1,10 @@
+<script>
+    export let progress = 0;
+</script>
 <style>
     .progress-container {
         margin: 20px 0;
-        border: solidrgb(194, 194, 194) 1px;
+        border: solid rgb(194, 194, 194) 1px;
     }
     .progress-bar {
         height: 30px;
@@ -11,9 +14,8 @@
 
 <div bp="grid">
     <div bp="offset-5@md 4@md 12@sm" class="progress-container">
-        <div class="progress-bar" style="width: 20%;">
-            <span class="sr-only">
-
+        <div class="progress-bar" style="width: {progress}%;">
+            <span class="sr-only">{progress}%
             </span>
         </div>
     </div>
